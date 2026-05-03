807a5b7c09dece248a497e132378a71b44f79f4a
germanmartinrodriguez-creator <germanmartinrodriguez@gmail.com>
2026-05-04 01:51:36 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,44 +1,12 @@
-const CACHE_NAME = 'calendario-barcelona-pro-v2';
-const APP_SHELL = [
-  './',
-  './index.html',
-  './manifest.json',
-  './favicon.png',
-  './icon-48.png',
-  './icon-72.png',
-  './icon-96.png',
-  './icon-128.png',
-  './icon-144.png',
-  './icon-152.png',
-  './icon-180.png',
-  './icon-192.png',
-  './icon-384.png',
-  './icon-512.png',
-  './icono.png'
-];
-
+const CACHE_NAME = 'calendario-barcelona-v5';
+const ASSETS = ['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', event => {
   self.skipWaiting();
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
-
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
-
 self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      if (cached) return cached;
-      return fetch(event.request).then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-        return response;
-      }).catch(() => caches.match('./index.html'));
-    })
-  );
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
 });
