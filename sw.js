463174ed5c45a89ce03724a93fa3a0960fbd8de4
germanmartinrodriguez-creator <germanmartinrodriguez@gmail.com>
2026-05-04 02:29:30 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,23 @@
-const CACHE_NAME = 'calendario-barcelona-ultra-v3';
-const ASSETS = ['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE_NAME = 'calendario-barcelona-final-real-v1';
+const ASSETS = [
+  './',
+  './index.html?v=final-real-1',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icono.png'
+];
 self.addEventListener('install', event => {
   self.skipWaiting();
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS).catch(()=>{})));
 });
 self.addEventListener('activate', event => {
   event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
+  event.respondWith(fetch(event.request).then(response => {
+    const clone = response.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone)).catch(()=>{});
+    return response;
+  }).catch(() => caches.match(event.request).then(r => r || caches.match('./index.html?v=final-real-1'))));
 });
