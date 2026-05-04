6e9b0af3ca253e2aa8dea8efaa1ae88cc4a1e63a
germanmartinrodriguez-creator <germanmartinrodriguez@gmail.com>
2026-05-04 02:00:02 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'calendario-barcelona-v5';
+const CACHE_NAME = 'calendario-barcelona-v6-vivo';
 const ASSETS = ['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', event => {
   self.skipWaiting();
