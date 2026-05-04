4c6dac84841204ac26cf9e34740cf13fa146717a
germanmartinrodriguez-creator <germanmartinrodriguez@gmail.com>
2026-05-04 02:38:35 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,23 +1,30 @@
-const CACHE_NAME = 'calendario-barcelona-final-real-v1';
+const CACHE_NAME = 'calendario-barcelona-final-definitiva-v2';
 const ASSETS = [
-  './',
-  './index.html?v=final-real-1',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  './icono.png'
+  '/CALENDARIO-ANDROID/',
+  '/CALENDARIO-ANDROID/index.html',
+  '/CALENDARIO-ANDROID/manifest.json',
+  '/CALENDARIO-ANDROID/icons/icon-192.png',
+  '/CALENDARIO-ANDROID/icons/icon-512.png',
+  '/CALENDARIO-ANDROID/icono.png'
 ];
 self.addEventListener('install', event => {
   self.skipWaiting();
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS).catch(()=>{})));
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(()=>{}));
 });
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k === CACHE_NAME ? null : caches.delete(k)))).then(() => self.clients.claim()));
 });
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request).then(response => {
-    const clone = response.clone();
-    caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone)).catch(()=>{});
-    return response;
-  }).catch(() => caches.match(event.request).then(r => r || caches.match('./index.html?v=final-real-1'))));
+  const req = event.request;
+  if (req.mode === 'navigate') {
+    event.respondWith(fetch(req).then(res => {
+      caches.open(CACHE_NAME).then(cache => cache.put('/CALENDARIO-ANDROID/index.html', res.clone())).catch(()=>{});
+      return res;
+    }).catch(() => caches.match('/CALENDARIO-ANDROID/index.html')));
+    return;
+  }
+  event.respondWith(caches.match(req).then(cached => cached || fetch(req).then(res => {
+    caches.open(CACHE_NAME).then(cache => cache.put(req, res.clone())).catch(()=>{});
+    return res;
+  }).catch(()=>cached)));
 });
